d5923d2d0b5ad00fdbb51d11d457a8d859df0402
rozzy <rosekaremeri@gmail.com>
2024-10-08 21:39:56 +0300

    Feature: lisk set up

--- eth_savings_project/smart_contracts/hardhat.config.ts
+++ eth_savings_project/smart_contracts/hardhat.config.ts
@@ -1,8 +1,32 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require('dotenv').config();
+
+// Add the following information after the "networks" configuration of the HardhatUserConfig
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  // Hardhat expects etherscan here, even if you're using Blockscout.
+  etherscan: {
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+    apiKey: {
+      "lisk-sepolia": "123"
+    },
+    customChains: [
+      {
+          network: "lisk-sepolia",
+          chainId: 4202,
+          urls: {
+              apiURL: "https://sepolia-blockscout.lisk.com/api",
+              browserURL: "https://sepolia-blockscout.lisk.com"
+          }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: false
+  },
 };
 
 export default config;
+
+// npx hardhat verify --network lisk-sepolia <deployed address>
